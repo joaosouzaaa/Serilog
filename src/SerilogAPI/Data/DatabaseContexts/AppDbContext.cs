0d65e1ce9db5f756bcad9849aaf555ae9f5f5242
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-13 21:41:04 -0300

    Feat: Adding DbContext and ConnectionFactory.

---
+++ src/SerilogAPI/Data/DatabaseContexts/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SerilogAPI.Data.DatabaseContexts;
+
+public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+}
