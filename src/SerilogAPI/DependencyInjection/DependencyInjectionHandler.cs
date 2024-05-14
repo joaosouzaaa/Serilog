768f65222bfde309aceafbb8e50f8e369d9c7de0
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-13 21:27:07 -0300

    Feat: Adding NotificationHandler.

--- src/SerilogAPI/DependencyInjection/DependencyInjectionHandler.cs
+++ src/SerilogAPI/DependencyInjection/DependencyInjectionHandler.cs
@@ -5,5 +5,6 @@ internal static class DependencyInjectionHandler
     internal static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCorsDependencyInjection();
+        services.AddSettingsDependencyInjection();
     }
 }
---
+++ src/SerilogAPI/Interfaces/Settings/INotificationHandler.cs
@@ -0,0 +1,10 @@
+﻿using SerilogAPI.Settings.NotificationSettings;
+
+namespace SerilogAPI.Interfaces.Settings;
+
+public interface INotificationHandler
+{
+    List<Notification> GetNotifications();
+    bool HasNotifications();
+    void AddNotification(string key, string message);
+}
