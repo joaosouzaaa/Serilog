562719c8d02dda5c87b5ba772640422660299201
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-14 22:14:06 -0300

    Feat: Adding PersonValidator.

---
+++ src/SerilogAPI/Validators/PersonValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SerilogAPI.Entities;
+
+namespace SerilogAPI.Validators;
+
+public sealed class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(p => p.Age).GreaterThan(0);
+
+        RuleFor(p => p.Name).Length(3, 100);
+    }
+}
