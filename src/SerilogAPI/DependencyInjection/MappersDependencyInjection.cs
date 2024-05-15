7783c0e2412b0447a8c62b0ef6297c4f84452cda
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-14 22:23:38 -0300

    Feat: Adding PersonMapper.

---
+++ src/SerilogAPI/DependencyInjection/MappersDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using SerilogAPI.Interfaces.Mappers;
+using SerilogAPI.Mappers;
+
+namespace SerilogAPI.DependencyInjection;
+
+internal static class MappersDependencyInjection
+{
+    internal static void AddMappersDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IPersonMapper, PersonMapper>();
+    }
+}
---
+++ src/SerilogAPI/Interfaces/Mappers/IPersonMapper.cs
@@ -0,0 +1,12 @@
+﻿using SerilogAPI.DataTransferObjects.Person;
+using SerilogAPI.Entities;
+
+namespace SerilogAPI.Interfaces.Mappers;
+
+public interface IPersonMapper
+{
+    Person SaveToDomain(PersonSave personSave);
+    void UpdateToDomain(PersonUpdate personUpdate, Person person);
+    PersonResponse DomainToResponse(Person person);
+    List<PersonResponse> DomainListToResponseList(List<Person> personList)
+}
