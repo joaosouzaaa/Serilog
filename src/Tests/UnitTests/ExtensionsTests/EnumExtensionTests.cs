11948339d7afd47e672359bad1e8c03482d9bdc2
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-15 21:16:54 -0300

    Tests: Extensions UnitTests.

---
+++ src/Tests/UnitTests/ExtensionsTests/EnumExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using SerilogAPI.Enums;
+using SerilogAPI.Extensions;
+
+namespace UnitTests.ExtensionsTests;
+
+public sealed class EnumExtensionTests
+{
+    [Fact]
+    public void Description_SuccessfulScenario_ReturnsEnumDescription()
+    {
+        // A
+        var logs = ELogs.Invalid;
+
+        const string expectedDescription = "{0} is Invalid.";
+
+        // A
+        var descriptionResult = logs.Description();
+
+        // A
+        Assert.Equal(expectedDescription, descriptionResult);
+    }
+}
