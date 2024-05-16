af55b2052990c029a482451c3b55c11915d2ff66
joaosouzaaa <joaoasouza982@gmail.com>
2024-05-15 22:56:37 -0300

    Feat: Adding Serilog.

---
+++ src/SerilogAPI/DependencyInjection/SerilogDependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using System.Reflection;
+
+namespace SerilogAPI.DependencyInjection;
+
+internal static class SerilogDependencyInjection
+{
+    internal static void AddSerilogDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSerilog(loggerConfiguration =>
+            loggerConfiguration.ReadFrom.Configuration(configuration));
+
+        //services.AddSerilog(loggerConfiguration =>
+        //    new LoggerConfiguration()
+        //        .MinimumLevel.Debug()
+        //        .WriteTo.Console()
+        //        .WriteTo.File(
+        //            configuration["Serilog:WriteTo:Args:path"]!,
+        //            rollingInterval: RollingInterval.Day,
+        //            rollOnFileSizeLimit: true)
+        //        .Enrich.WithThreadId()
+        //        .Enrich.WithClientIp()
+        //        .Enrich.WithCorrelationId()
+        //        .Enrich.WithProcessId()
+        //        .Enrich.FromLogContext());
+    }
+}
